571210b4e95b872b79adbc7ef8c66864085b3896
Adriandlph <52699504+adriandlph@users.noreply.github.com>
2026-07-06 11:35:03 -0700

    Fix: first execution errors

--- src/main/java/com/adlph/internal/managment/index/server/IndexServerApplication.java
+++ src/main/java/com/adlph/internal/managment/index/server/IndexServerApplication.java
@@ -3,10 +3,25 @@ package com.adlph.internal.managment.index.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 public class IndexServerApplication {
 
 	public static void main(String[] args) {
+		var baseDir = Paths.get("").toAbsolutePath().normalize();
+		var dirs = new String[]{"db", "logs"};
+		for (var dir : dirs) {
+			var path = baseDir.resolve(dir);
+			if (Files.notExists(path)) {
+				try {
+					Files.createDirectories(path);
+				} catch (Exception e) {
+					System.err.println("Warning: Could not create directory " + path + ": " + e.getMessage());
+				}
+			}
+		}
 		SpringApplication.run(IndexServerApplication.class, args);
 	}
 
