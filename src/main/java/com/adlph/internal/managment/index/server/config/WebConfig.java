1986b8f6202219d62db9a25805b74aa3dd5c93a1
Adriandlph <52699504+adriandlph@users.noreply.github.com>
2026-07-08 10:39:21 -0700

    Fix error

--- src/main/java/com/adlph/internal/managment/index/server/config/WebConfig.java
+++ src/main/java/com/adlph/internal/managment/index/server/config/WebConfig.java
@@ -1,6 +1,9 @@
 package com.adlph.internal.managment.index.server.config;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.tomcat.servlet.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.servlet.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -13,6 +16,28 @@ public class WebConfig {
 
     @Value("${cors.allowed-origins:http://localhost:5173,http://127.0.0.1:5173}")
     private String allowedOrigins;
+
+    @Value("${server.http.port:8080}")
+    private int httpPort;
+
+    @Value("${server.port:9000}")
+    private int httpsPort;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalConnectors(createHttpRedirectConnector());
+        return tomcat;
+    }
+
+    private Connector createHttpRedirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(httpPort);
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setRedirectPort(httpsPort);
+        return connector;
+    }
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
